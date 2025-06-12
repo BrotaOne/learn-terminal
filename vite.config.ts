de38a8e0c003815c2c99939639cf604ae35e898e
Brota <vegetaren1@gmail.com>
2025-06-12 15:55:02 +0800

    dev: 增加env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/learn-terminal',
+  base: process.env.target === 'vercel' ? '/' : '/learn-terminal',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
